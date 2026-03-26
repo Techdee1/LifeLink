ff0d075392856f6d1e7c0cdff6a0fd2ecc154062
Akeem Jr Odebiyi <160186292+Techdee1@users.noreply.github.com>
2026-03-26 14:51:21 +0000

    Fix LoanRepository bridged amount JPQL

--- src/main/java/org/interswitch/app/LifeLink/repository/LoanRepository.java
+++ src/main/java/org/interswitch/app/LifeLink/repository/LoanRepository.java
@@ -3,12 +3,11 @@ package org.interswitch.app.LifeLink.repository;
 import org.interswitch.app.LifeLink.model.Loan;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 
 import java.math.BigDecimal;
 
 public interface LoanRepository extends JpaRepository<Loan, Long> {
 
-    @Query("SELECT COALESCE(SUM(p.bridgedAmount), 0) FROM Payment p")
+    @Query("SELECT COALESCE(SUM(l.bridgedAmount), 0) FROM Loan l")
     BigDecimal findSumBridgedAmount();
 }
